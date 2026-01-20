7843c053cbe7e71398d5b7722c0aef625ce7d9df
Ian Ker-Seymer <ian.kerseymer@shopify.com>
2026-01-20 18:14:01 -0500

    Add Ruby 4.1 cfg declarations to build.rs

--- build.rs
+++ build.rs
@@ -1,4 +1,8 @@
 fn main() -> Result<(), Box<dyn std::error::Error>> {
+    // Declare Ruby 4.1 cfg flags
+    println!("cargo::rustc-check-cfg=cfg(ruby_lt_4_1)");
+    println!("cargo::rustc-check-cfg=cfg(ruby_gte_4_1)");
+
     let _ = rb_sys_env::activate()?;
 
     Ok(())
