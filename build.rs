7ba79587a8d13f03fded856af0d3956d75919a53
Ian Ker-Seymer <ian.kerseymer@shopify.com>
2026-01-20 18:13:33 -0500

    Fix Ruby 4.1 typed_data compatibility

--- build.rs
+++ build.rs
@@ -8,6 +8,9 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
     println!("cargo:rustc-check-cfg=cfg(ruby_gte_3_2)");
     println!("cargo:rustc-check-cfg=cfg(ruby_lt_3_3)");
     println!("cargo:rustc-check-cfg=cfg(ruby_gte_3_3)");
+    // Declare Ruby 4.1 cfg flags
+    println!("cargo:rustc-check-cfg=cfg(ruby_lt_4_1)");
+    println!("cargo:rustc-check-cfg=cfg(ruby_gte_4_1)");
 
     let _ = rb_sys_env::activate()?;
 
