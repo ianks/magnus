1b666736c3fad79109c05db47f02a5df38ca8b79
Ian Ker-Seymer <ian.kerseymer@shopify.com>
2026-01-20 18:12:26 -0500

    Fix Ruby 4.1 typed_data compatibility

--- src/typed_data.rs
+++ src/typed_data.rs
@@ -343,11 +343,11 @@ where
                 dsize,
                 #[cfg(ruby_gte_2_7)]
                 dcompact,
-                #[cfg(all(ruby_gte_2_7, not(ruby_gt_4_0)))]
+                #[cfg(all(ruby_gte_2_7, ruby_lt_4_1))]
                 reserved: [ptr::null_mut(); 1],
                 #[cfg(ruby_lt_2_7)]
                 reserved: [ptr::null_mut(); 2],
-                #[cfg(ruby_gt_4_0)]
+                #[cfg(ruby_gte_4_1)]
                 handle_weak_references: None,
             },
             parent: ptr::null(),
