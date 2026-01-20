97c478df6f2c84671d51b425f50b38ac59e39b8a
Ian Ker-Seymer <ian.kerseymer@shopify.com>
2026-01-20 18:21:21 -0500

    Fix Ruby 4.1 struct to include both handle_weak_references and reserved

--- src/typed_data.rs
+++ src/typed_data.rs
@@ -349,10 +349,12 @@ where
                 dfree,
                 dsize,
                 dcompact,
+                #[cfg(ruby_gte_4_1)]
+                handle_weak_references: None,
                 #[cfg(ruby_lt_4_1)]
                 reserved: [ptr::null_mut(); 1],
                 #[cfg(ruby_gte_4_1)]
-                handle_weak_references: None,
+                reserved: [ptr::null_mut(); 7],
             },
             parent: ptr::null(),
             data: ptr::null_mut(),
